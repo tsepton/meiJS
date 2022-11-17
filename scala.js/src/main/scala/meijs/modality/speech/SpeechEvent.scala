6c6bbf7058b127915d39146f453ddc1fd242a6d6
Thibaut Septon <thibaut.septon@unamur.be>
2022-11-17 12:48:26 +0100

    Add SpeechEvent representation (missing file)

---
+++ scala.js/src/main/scala/meijs/modality/speech/SpeechEvent.scala
@@ -0,0 +1,35 @@
+package meijs.modality.speech
+
+import meijs.eventbase.structures.AtomicEvent
+import meijs.modality.Modality
+import meijs.modality.speech.web_speech_api.{
+  SpeechRecognitionResult,
+  SpeechRecognitionResultList
+}
+
+import scala.language.implicitConversions
+
+sealed trait SpeechEvent extends AtomicEvent {
+  override val name: String
+  override val modality: Modality = Modality.Voice
+}
+
+object SpeechEvent {
+
+  def from(result: SpeechRecognitionResult): List[SpeechEvent] =
+    result.flatMap(alt =>
+      alt.transcript.split(" ").toList match {
+        case words if words.length == 1 => List(Word(words.head))
+        case words =>
+          List(List(Sentence(alt.transcript)), words.map(str => Word(str))).flatten
+      }
+    )
+
+  def from(results: SpeechRecognitionResultList): List[SpeechEvent] =
+    results.flatMap(result => from(result))
+
+}
+
+case class Word(name: String) extends SpeechEvent
+
+case class Sentence(name: String) extends SpeechEvent
