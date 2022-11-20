6453cbdf39ff9b37ffccb7d22d46751041165fea
Thibaut Septon <tsepton@gmail.com>
2022-11-20 16:21:36 +0100

    Add proof of concept

--- framework-test/src/App.svelte
+++ framework-test/src/App.svelte
@@ -4,10 +4,48 @@
   meiJS.enable({
     useEventSystem: true,
   });
+
+  document.addEventListener("put-that-there", (e) => {
+    console.log("putting that there...");
+    const clicks = e.source.occurrence.filter((occ) => occ.name === "click");
+    const colours = clicks.map((click) => click.target.style.backgroundColor);
+    clicks[0].target.style.backgroundColor = colours[1];
+    clicks[1].target.style.backgroundColor = colours[0];
+  });
+
+  document.addEventListener("update-that-color", (e) => {
+    console.log("updating color...");
+    e.source.occurrence
+      .filter((occ) => occ.name === "click")
+      .forEach((clickEvent) => {
+        clickEvent.target.style.backgroundColor = randomColor();
+      });
+  });
+
+  function randomColor() {
+    return (
+      "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0")
+    );
+  }
 </script>
 
 <main>
-  {#each Object.keys(meiJS) as ex}
-    <div>{ex}</div>
-  {/each}
+  <div class="container">
+    {#each [...Array(96).keys()] as i}
+      <div class="element" />
+    {/each}
+  </div>
 </main>
+
+<style>
+  .container {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+  }
+  .element {
+    width: 10vw;
+    height: 10vh;
+    border: 1px solid grey;
+  }
+</style>
---
+++ framework-test/src/component/ExportedLib.svelte
@@ -0,0 +1,7 @@
+<script>
+  import * as meiJS from "@tsepton/MeiJS";
+</script>
+
+{#each Object.keys(meiJS) as ex}
+  <div>{ex}</div>
+{/each}
